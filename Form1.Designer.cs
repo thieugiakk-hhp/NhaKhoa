50d47c2fd0b892760914c605b8a9101b220e1931
Hoàng Hồng Phong <thieugiakk.hhp@gmail.com>
2021-08-09 11:29:22 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace NhaKhoa
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.soRangNho = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tongTien = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.caoVoiRang = new System.Windows.Forms.CheckBox();
+            this.tramRang = new System.Windows.Forms.CheckBox();
+            this.hoTenKH = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTINHTIENNHAKHOA = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.soRangNho)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // soRangNho
+            // 
+            this.soRangNho.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.soRangNho.Location = new System.Drawing.Point(164, 226);
+            this.soRangNho.Margin = new System.Windows.Forms.Padding(2);
+            this.soRangNho.Name = "soRangNho";
+            this.soRangNho.Size = new System.Drawing.Size(50, 24);
+            this.soRangNho.TabIndex = 28;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button1.Location = new System.Drawing.Point(200, 318);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 35);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Tính Tiền";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tongTien
+            // 
+            this.tongTien.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.tongTien.Location = new System.Drawing.Point(165, 277);
+            this.tongTien.Margin = new System.Windows.Forms.Padding(2);
+            this.tongTien.Name = "tongTien";
+            this.tongTien.Size = new System.Drawing.Size(259, 26);
+            this.tongTien.TabIndex = 26;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Location = new System.Drawing.Point(44, 278);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 20);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "Tổng Tiền:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label7.Location = new System.Drawing.Point(228, 229);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label7.Size = new System.Drawing.Size(190, 18);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "(đơn giá 100.000 vnd / răng)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label6.Location = new System.Drawing.Point(44, 226);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 20);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Nhổ Răng";
+            // 
+            // caoVoiRang
+            // 
+            this.caoVoiRang.AutoSize = true;
+            this.caoVoiRang.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.caoVoiRang.Location = new System.Drawing.Point(164, 186);
+            this.caoVoiRang.Margin = new System.Windows.Forms.Padding(2);
+            this.caoVoiRang.Name = "caoVoiRang";
+            this.caoVoiRang.Size = new System.Drawing.Size(106, 21);
+            this.caoVoiRang.TabIndex = 22;
+            this.caoVoiRang.Text = "300.000 vnd";
+            this.caoVoiRang.UseVisualStyleBackColor = true;
+            // 
+            // tramRang
+            // 
+            this.tramRang.AutoSize = true;
+            this.tramRang.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.tramRang.Location = new System.Drawing.Point(164, 147);
+            this.tramRang.Margin = new System.Windows.Forms.Padding(2);
+            this.tramRang.Name = "tramRang";
+            this.tramRang.Size = new System.Drawing.Size(106, 21);
+            this.tramRang.TabIndex = 21;
+            this.tramRang.Text = "200.000 vnd";
+            this.tramRang.UseVisualStyleBackColor = true;
+            // 
+            // hoTenKH
+            // 
+            this.hoTenKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.hoTenKH.Location = new System.Drawing.Point(164, 59);
+            this.hoTenKH.Margin = new System.Windows.Forms.Padding(2);
+            this.hoTenKH.Name = "hoTenKH";
+            this.hoTenKH.Size = new System.Drawing.Size(260, 26);
+            this.hoTenKH.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label5.Location = new System.Drawing.Point(45, 147);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 18);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Trám Răng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label4.Location = new System.Drawing.Point(45, 186);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 18);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Cạo Vôi";
+            // 
+            // lblTINHTIENNHAKHOA
+            // 
+            this.lblTINHTIENNHAKHOA.AutoSize = true;
+            this.lblTINHTIENNHAKHOA.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblTINHTIENNHAKHOA.Location = new System.Drawing.Point(160, 15);
+            this.lblTINHTIENNHAKHOA.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTINHTIENNHAKHOA.Name = "lblTINHTIENNHAKHOA";
+            this.lblTINHTIENNHAKHOA.Size = new System.Drawing.Size(203, 25);
+            this.lblTINHTIENNHAKHOA.TabIndex = 17;
+            this.lblTINHTIENNHAKHOA.Text = "Tính Tiền Nha Khoa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(44, 111);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Dịch Vụ Sử Dụng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(44, 63);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Họ Tên:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 376);
+            this.Controls.Add(this.soRangNho);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tongTien);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.caoVoiRang);
+            this.Controls.Add(this.tramRang);
+            this.Controls.Add(this.hoTenKH);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTINHTIENNHAKHOA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.soRangNho)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown soRangNho;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tongTien;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox caoVoiRang;
+        private System.Windows.Forms.CheckBox tramRang;
+        private System.Windows.Forms.TextBox hoTenKH;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTINHTIENNHAKHOA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
